fd03d065261666d65463cd044c11e849bc067910
Arshitsharma <arshitsharma2020@gamil.com>
2023-12-22 17:45:28 +0530

    ALMOST

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\5th sem\spam detector\venv
+set VIRTUAL_ENV=D:\5th sem\spam-detector\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
