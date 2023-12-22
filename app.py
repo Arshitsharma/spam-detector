cfcfd0cdc8d10d7c42f4a4c31f8469d76b9ab5e5
Arshitsharma <arshitsharma2020@gamil.com>
2023-12-22 10:56:41 +0530

    demo

---
+++ app.py
@@ -0,0 +1,53 @@
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+
+# Load the dataset
+df_sms = pd.read_csv('spam.csv', encoding='latin-1')
+# Data preprocessing steps
+df_sms = df_sms.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
+df_sms = df_sms.rename(columns={"v1": "label", "v2": "sms"})
+
+# Convert 'label' to numerical values
+df_sms['label'] = df_sms['label'].map({'ham': 0, 'spam': 1})
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df_sms['sms'], df_sms['label'], test_size=0.20, random_state=1)
+
+# Create and train the model
+count_vector = CountVectorizer()
+training_data = count_vector.fit_transform(X_train)
+testing_data = count_vector.transform(X_test)
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+# Home page route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']
+    transformed_message = count_vector.transform([message])
+    prediction = naive_bayes.predict(transformed_message)
+    if prediction[0] == 1:
+        result = 'Spam'
+    else:
+        result = 'Not Spam'
+    return jsonify({'prediction': result})  # Respond with the prediction result in JSON format
+
+# Route for serving static files (CSS, JS, etc.)
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,108 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutText = "Welcome to the Spam Detector project! Our goal is to provide a tool that helps identify and filter out spam messages. Using advanced machine learning algorithms, our system analyzes the content of messages to determine the likelihood of being spam. With the Spam Detector, users can submit messages for analysis and receive predictions on whether the message might be spam or not. Feel free to try it out by entering a message in the form above and see the prediction results!";
+    const featuresText = "Our Spam Detector includes various features such as advanced algorithms, real-time analysis, and a user-friendly interface to help users in identifying spam messages effectively.";
+    const howItWorksText = "Implemented within Flask, our Spam Detector leverages the Multinomial Naive Bayes algorithm, known for its efficiency in text classification, particularly with word frequency features. This choice stems from its simplicity and effectiveness in handling large feature spaces, enabling accurate categorization of messages into spam and non-spam categories based on their textual content. Flask's framework efficiently manages the backend, ensuring swift identification and empowering users with effective spam filtration.";
+
+    const aboutTypingText = document.getElementById('about-typing-text');
+    const featuresTypingText = document.getElementById('features-typing-text');
+    const howItWorksTypingText = document.getElementById('how-it-works-typing-text');
+
+    let aboutIndex = 0;
+    let featuresIndex = 0;
+    let howItWorksIndex = 0;
+
+    let typingStarted = {
+        about: false,
+        features: false,
+        howItWorks: false
+    };
+
+function typeWriter(element, text, index) {
+    if (index <= text.length) {
+        const textWithCursor = text.substring(0, index) + '<span class="cursor">|</span>'; // Adding a cursor '|' at the current index
+        element.innerHTML = textWithCursor;
+        index++;
+        setTimeout(() => typeWriter(element, text, index), 20); // Adjust the typing speed here (milliseconds)
+    }
+}
+
+    
+
+    function startTypingEffect() {
+        checkAndStartTyping('about', aboutText, aboutTypingText, aboutIndex);
+        checkAndStartTyping('features', featuresText, featuresTypingText, featuresIndex);
+        checkAndStartTyping('how-it-works', howItWorksText, howItWorksTypingText, howItWorksIndex);
+    }
+
+    function checkAndStartTyping(section, text, element, index) {
+        const sectionElement = document.getElementById(section);
+        const sectionPosition = sectionElement.getBoundingClientRect();
+
+        if (
+            sectionPosition.top >= 0 &&
+            sectionPosition.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+            && !typingStarted[section]
+        ) {
+            typingStarted[section] = true;
+            typeWriter(element, text, index);
+        }
+    }
+
+    window.addEventListener('scroll', startTypingEffect);
+    startTypingEffect(); // Initiate on page load
+});
+
+
+
+
+
+function showPrediction(event) {
+    event.preventDefault();
+    const message = document.getElementById('message').value;
+
+    fetch('/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `message=${encodeURIComponent(message)}`,
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('prediction-result').innerText = `The message is: ${data.prediction}`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function resetForm() {
+    document.getElementById("message").value = ""; // Reset the textarea
+    document.getElementById("prediction-result").innerText = ""; // Clear the prediction result
+}
+
+// script.js
+
+
+let header = document.getElementById("header");
+let hoveringAtTop = false;
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 50 && !hoveringAtTop) {
+        header.style.top = "-80px"; // Hide the header when not at the top and not hovering
+    } else {
+        header.style.top = "0"; // Show the header otherwise
+    }
+}
+
+header.addEventListener("mouseenter", function() {
+    hoveringAtTop = true;
+    header.style.top = "0"; // Show the header when hovering at the top
+});
+
+header.addEventListener("mouseleave", function() {
+    hoveringAtTop = false;
+    header.style.top = "-80px"; // Hide the header when not hovering
+});
